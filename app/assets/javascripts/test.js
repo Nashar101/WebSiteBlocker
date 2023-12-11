bde4d8d9272d025bd9f2a2e99d678301667a340e
Gilead <nashar101@gmail.com>
2023-12-11 23:20:32 +0000

    change gemfile ruby version to laptop ruby version when you pull tomorrow

--- app/assets/javascripts/test.js
+++ app/assets/javascripts/test.js
@@ -1,15 +1,54 @@
 function SayHI(){
     console.log("hi")
 }
-
-function decrement(idvalue){
-     console.log("decrement called on " +idvalue)
-     let value = document.getElementById('seconds ' + idvalue).innerHTML
-     setInterval(function() {
-         value = (value)-1; document.getElementById('seconds ' +idvalue).innerHTML = value }, 1000)
+function decrementDays(idvalue, duedate){
+    let value = document.getElementById('days ' + idvalue).innerHTML
+    setInterval(function() {
+        var d = Math.floor((new Date(duedate)-Date.now())/1000)
+        value = display_days(d); document.getElementById('days ' +idvalue).innerHTML = value }, 1000)
 }
 
-function startTimer(){
-    var trArray = []
+function decrementHours(idvalue, duedate){
+    let value = document.getElementById('hours ' + idvalue).innerHTML
+    setInterval(function() {
+        var d = Math.floor((new Date(duedate)-Date.now())/1000)
+        value = display_hours(d); document.getElementById('hours ' +idvalue).innerHTML = value }, 1000)
 }
 
+function decrementMinutes(idvalue, duedate){
+    let value = document.getElementById('minutes ' + idvalue).innerHTML
+    setInterval(function() {
+        var d = Math.floor((new Date(duedate)-Date.now())/1000)
+        value = display_minutes(d); document.getElementById('minutes ' +idvalue).innerHTML = value }, 0)
+}
+
+function decrementSeconds(idvalue, duedate){
+    console.log("decrement called on " +idvalue)
+    let value = document.getElementById('seconds ' + idvalue).innerHTML
+    setInterval(function() {
+        var d = Math.floor((new Date(duedate)-Date.now())/1000)
+        value = display_seconds(d);
+        /*console.log(display_minutes(d))
+        console.log(display_hours(d))
+        console.log(display_days(d))*/
+        if(display_days(d) <= 0 && display_minutes(d) <= 0 && display_hours(d) <= 0 && display_days(d)){
+            console.log("im in here")
+            document.getElementById('delete ' + idvalue).click()
+            return;
+        }
+        document.getElementById('seconds ' +idvalue).innerHTML = value }, 1000)
+}
+function display_days(duedate){
+    return Math.floor(duedate / (3600 * 24));
+}
+
+function display_hours(duedate){
+    return Math.floor((duedate % (3600 * 24)) / 3600);
+}
+function display_minutes(duedate){
+    return Math.floor((duedate % 3600) / 60);
+}
+
+function display_seconds(duedate){
+   return  duedate%60;
+}
--- app/models/blockedsite.rb
+++ app/models/blockedsite.rb
@@ -10,4 +10,11 @@ class Blockedsite < ApplicationRecord
     (((duedate - Time.now)-((duedate - Time.now)/60/60/24).to_i*24*60*60)/60/60).round
   end
 
+  def display_minutes
+    (((duedate-Time.now)-(display_days*60*60*24+display_hours*60*60))/60).round
+  end
+
+  def display_seconds
+    ((duedate)-((display_days.to_i*24*60*60)+(display_hours.to_i*60*60)+(display_minutes.to_i*60))).to_i.round
+  end
 end
